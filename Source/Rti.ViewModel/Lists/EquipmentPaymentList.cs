c12e34893275eb3d2ef09a3bbc6969f4baaf5f5a
EugeneMukhamedshin <MukhamedshinER@gmail.com>
2016-10-13 22:40:59 +0300

    -

--- Source/Rti.ViewModel/Lists/EquipmentPaymentList.cs
+++ Source/Rti.ViewModel/Lists/EquipmentPaymentList.cs
@@ -22,8 +22,7 @@ namespace Rti.ViewModel.Lists
 
         public EquipmentPaymentList(bool editMode, IViewService viewService, IRepositoryFactory repositoryFactory)
             : base(editMode, viewService, repositoryFactory)
-        {
-            TypeMaps.Add(new Tuple<Type, Type>(typeof(EquipmentPaymentViewModel), typeof(EditViewModel.EquipmentPaymentEdit)));
+        {TypeMaps.Add(new Tuple<Type, Type>(typeof(EquipmentPaymentViewModel), typeof(EditViewModel.EquipmentPaymentEdit)));
 
             AddEquipmentPaymentCommand = new DelegateCommand(
                 "Добавить оплату",
